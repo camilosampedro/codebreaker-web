dedd3323f7f64db66c08437f05fbea4e8ccb8c5d
Camilo Sampedro <sampedro1903@gmail.com>
2017-04-22 10:35:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DojoTDD
+
+## Integrantes
+ - Sara Tamayo
+ - Camilo Sampedro
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "net/http"
+  "os"
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+  port := os.Getenv("PORT")
+
+  if port == "" {
+    port = "8081"
+  }
+
+  router := gin.Default()
+
+  // This handler will match /user/john but will not match neither /user/ or /user
+	router.GET("/codebreaker/setup/:number", func(c *gin.Context) {
+		number := c.Param("number")
+    setSecret(number)
+		c.String(http.StatusOK, "Secret set: %s", number)
+	})
+
+  // This handler will match /user/john but will not match neither /user/ or /user
+	router.GET("/codebreaker/guess/:number", func(c *gin.Context) {
+		number := c.Param("number")
+    result := codebreaker(number)
+		c.String(http.StatusOK, "Answer: %s", result)
+	})
+
+  router.Run(":" + port)
+}
